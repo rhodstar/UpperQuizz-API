5fa22b9965372ce52fc77c7281faeabffe4d1d72
rhodstar <rhodfra@gmail.com>
2021-08-23 20:10:12 -0500

    Added db creation scripts

---
+++ db/01-seeder.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import psycopg2
+import psycopg2.extras
+import os
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+with open("01-seeder.sql", "r") as f:
+    query = f.read()
+
+    conn = psycopg2.connect(DATABASE_URL)
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute(query)
