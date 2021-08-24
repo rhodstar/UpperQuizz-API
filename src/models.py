f8c1b62e886746fb50e86586a7ae21dd4184d4f6
rhodstar <rhodfra@gmail.com>
2021-08-23 22:43:39 -0500

    Fix null condition for aciertos totales

--- src/models.py
+++ src/models.py
@@ -83,9 +83,9 @@ def get_user_evaluations(user):
         res_dict = []
         for r in res:
             item_dict = {}
-            print(r['aciertos_totales'])
             aciertos_totales = 0
-            if r['aciertos_totales'] not None: aciertos_totales = r['aciertos_totales']
+            if r['aciertos_totales'] not None: 
+                aciertos_totales = r['aciertos_totales']
             item_dict['evaluacion_id'] = r['evaluacion_id']  
             item_dict['examen_id'] = r['examen_id']  
             item_dict['status'] = r['nombre']
